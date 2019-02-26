c15c1ec96595f001631fed2cddd14c59bb826454
andrew-fallon <afallon@stanford.edu>
2019-02-26 15:40:15 -0800

    working receive

--- receive_2.py
+++ receive_2.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Receive 2
-# Generated: Tue Feb 26 13:46:25 2019
+# Generated: Tue Feb 26 15:37:25 2019
 ##################################################
 
 if __name__ == '__main__':
@@ -17,10 +17,12 @@ if __name__ == '__main__':
             print "Warning: failed to XInitThreads()"
 
 from PyQt4 import Qt
+from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
+from gnuradio.fft import logpwrfft
 from gnuradio.filter import firdes
 from optparse import OptionParser
 import osmosdr
@@ -60,7 +62,7 @@ class receive_2(gr.top_block, Qt.QWidget):
         self.sdr_samp = sdr_samp = 1.8e6
         self.samp_rate = samp_rate = 48e3
         self.fft_size = fft_size = 1024
-        self.center_freq = center_freq = 90100000
+        self.center_freq = center_freq = 430e6
 
         ##################################################
         # Blocks
@@ -84,6 +86,79 @@ class receive_2(gr.top_block, Qt.QWidget):
         
         
           
+        self.qtgui_number_sink_1 = qtgui.number_sink(
+            gr.sizeof_short,
+            0,
+            qtgui.NUM_GRAPH_HORIZ,
+            1
+        )
+        self.qtgui_number_sink_1.set_update_time(0.10)
+        self.qtgui_number_sink_1.set_title("")
+        
+        labels = ["", "", "", "", "",
+                  "", "", "", "", ""]
+        units = ["", "", "", "", "",
+                 "", "", "", "", ""]
+        colors = [("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
+                  ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
+        factor = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        for i in xrange(1):
+            self.qtgui_number_sink_1.set_min(i, -1)
+            self.qtgui_number_sink_1.set_max(i, 1)
+            self.qtgui_number_sink_1.set_color(i, colors[i][0], colors[i][1])
+            if len(labels[i]) == 0:
+                self.qtgui_number_sink_1.set_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_number_sink_1.set_label(i, labels[i])
+            self.qtgui_number_sink_1.set_unit(i, units[i])
+            self.qtgui_number_sink_1.set_factor(i, factor[i])
+        
+        self.qtgui_number_sink_1.enable_autoscale(False)
+        self._qtgui_number_sink_1_win = sip.wrapinstance(self.qtgui_number_sink_1.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_number_sink_1_win)
+        self.qtgui_number_sink_0 = qtgui.number_sink(
+            gr.sizeof_float,
+            0,
+            qtgui.NUM_GRAPH_HORIZ,
+            1
+        )
+        self.qtgui_number_sink_0.set_update_time(0.10)
+        self.qtgui_number_sink_0.set_title("")
+        
+        labels = ["", "", "", "", "",
+                  "", "", "", "", ""]
+        units = ["", "", "", "", "",
+                 "", "", "", "", ""]
+        colors = [("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
+                  ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
+        factor = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        for i in xrange(1):
+            self.qtgui_number_sink_0.set_min(i, -1)
+            self.qtgui_number_sink_0.set_max(i, 1)
+            self.qtgui_number_sink_0.set_color(i, colors[i][0], colors[i][1])
+            if len(labels[i]) == 0:
+                self.qtgui_number_sink_0.set_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_number_sink_0.set_label(i, labels[i])
+            self.qtgui_number_sink_0.set_unit(i, units[i])
+            self.qtgui_number_sink_0.set_factor(i, factor[i])
+        
+        self.qtgui_number_sink_0.enable_autoscale(False)
+        self._qtgui_number_sink_0_win = sip.wrapinstance(self.qtgui_number_sink_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_number_sink_0_win)
+        self.logpwrfft_x_0 = logpwrfft.logpwrfft_c(
+        	sample_rate=samp_rate,
+        	fft_size=fft_size,
+        	ref_scale=2,
+        	frame_rate=30,
+        	avg_alpha=1.0,
+        	average=False,
+        )
+        self.blocks_short_to_float_0 = blocks.short_to_float(1, float(fft_size/samp_rate))
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_short*1)
+        self.blocks_argmax_xx_0 = blocks.argmax_fs(fft_size)
         self.NANO_SDR = osmosdr.source( args="numchan=" + str(1) + " " + "rtl=0" )
         self.NANO_SDR.set_time_unknown_pps(osmosdr.time_spec_t())
         self.NANO_SDR.set_sample_rate(sdr_samp)
@@ -102,7 +177,13 @@ class receive_2(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.NANO_SDR, 0), (self.logpwrfft_x_0, 0))    
         self.connect((self.NANO_SDR, 0), (self.qtgui_sink_x_0, 0))    
+        self.connect((self.blocks_argmax_xx_0, 1), (self.blocks_null_sink_0, 0))    
+        self.connect((self.blocks_argmax_xx_0, 0), (self.blocks_short_to_float_0, 0))    
+        self.connect((self.blocks_argmax_xx_0, 1), (self.qtgui_number_sink_1, 0))    
+        self.connect((self.blocks_short_to_float_0, 0), (self.qtgui_number_sink_0, 0))    
+        self.connect((self.logpwrfft_x_0, 0), (self.blocks_argmax_xx_0, 0))    
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "receive_2")
@@ -123,20 +204,23 @@ class receive_2(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.qtgui_sink_x_0.set_frequency_range(self.center_freq, self.samp_rate)
+        self.logpwrfft_x_0.set_sample_rate(self.samp_rate)
+        self.blocks_short_to_float_0.set_scale(float(self.fft_size/self.samp_rate))
 
     def get_fft_size(self):
         return self.fft_size
 
     def set_fft_size(self, fft_size):
         self.fft_size = fft_size
+        self.blocks_short_to_float_0.set_scale(float(self.fft_size/self.samp_rate))
 
     def get_center_freq(self):
         return self.center_freq
 
     def set_center_freq(self, center_freq):
         self.center_freq = center_freq
-        self.qtgui_sink_x_0.set_frequency_range(self.center_freq, self.samp_rate)
         self.NANO_SDR.set_center_freq(self.center_freq, 0)
+        self.qtgui_sink_x_0.set_frequency_range(self.center_freq, self.samp_rate)
 
 
 def main(top_block_cls=receive_2, options=None):
